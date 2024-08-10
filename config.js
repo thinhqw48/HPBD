66596d22e0db2b661d36c899ccf1ad74da4d3e84
unknown <thinhqw48@gmail.com>
2024-08-11 02:42:43 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = '\n';
+const giftUrl = '';
+const eventName = 'Nhấn để mở';
+const titleCard = 'Tặng người ấy';
+const contentCard = "Tadaaa!! Hôm nay là kỉ niệm tròn 18 năm kể từ ngày công chúa của tui ra đời rồi nè:3 Chúc người yêu của tui một ngày sinh nhật tràn ngập niềm vui và những nụ cười. Tui mong em sẽ luôn là một người tích cực, vui vẻ, ít buồn hơn, ít suy nghĩ linh tinh hơn kể cả lúc có tui hay khong, hì. Chúc những điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau. Yêu Ngọc Châu!";
+const giftImage = 'ngocchau.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;

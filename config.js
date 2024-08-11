151633567eaba91507e99b180e89b1bace74b8a5
thinhqw48 <140641198+thinhqw48@users.noreply.github.com>
2024-08-11 08:42:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const nameGirl = '\n';
 const giftUrl = '';
 const eventName = 'Nhấn để mở';
-const titleCard = 'Tặng người ấy';
+const titleCard = 'Gửi đến người ấy';
 const contentCard = "Tadaaa!! Hôm nay là kỉ niệm tròn 18 năm kể từ ngày công chúa của tui ra đời rồi nè:3 Chúc người yêu của tui một ngày sinh nhật tràn ngập niềm vui và những nụ cười. Tui mong em sẽ luôn là một người tích cực, vui vẻ, ít buồn hơn, ít suy nghĩ linh tinh hơn kể cả lúc có tui hay khong, hì. Chúc những điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau. Yêu Ngọc Châu!";
 const giftImage = 'ngocchau.png';
 const base64 = '';
